a2da8381cdafeb2355d59f70399f049a3d0bb474
Stefan Hagen <stefan@hagen.link>
2022-08-01 16:55:18 +0200

    Bumped impl version for release

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -15,8 +15,8 @@ DEFAULT_CONFIG_NAME = '.liitos.json'
 DEFAULT_LF_ONLY = 'YES'
 
 # [[[fill git_describe()]]]
-__version__ = '2022.7.28+parent.9fb117a4'
-# [[[end]]] (checksum: a143e8c8551090fa81324b0683ba1a40)
+__version__ = '2022.8.1+parent.eb3805a3'
+# [[[end]]] (checksum: 5914f765c8db0334c56fd55d8685d046)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
