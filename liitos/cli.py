756aab90b7e3cdf495d32c8324d1ef570b2ffd91
Stefan Hagen <stefan@hagen.link>
2022-11-24 20:52:23 +0100

    Refactored commands to reuse the standard arguments and options as well as share the verification

--- liitos/cli.py
+++ liitos/cli.py
@@ -1,8 +1,6 @@
 """Command line interface for splice (Finnish liitos) contributions."""
-import argparse
 import pathlib
 import sys
-from typing import List, Union
 
 import typer
 
@@ -15,12 +13,49 @@ import liitos.labels as lab
 import liitos.meta as met
 import liitos.patch as pat
 import liitos.tables as tab
-from liitos import APP_ALIAS, APP_NAME, DEBUG, QUIET, VERBOSE, __version__ as APP_VERSION
+from liitos import APP_ALIAS, APP_NAME, DEBUG, QUIET, VERBOSE, __version__ as APP_VERSION, log
 
 app = typer.Typer(
     add_completion=False,
     context_settings={'help_option_names': ['-h', '--help']},
     no_args_is_help=True,
+)
+
+DocumentRoot = typer.Option(
+    '',
+    '-d',
+    '--document-root',
+    help='Root of the document tree to visit. Optional\n(default: positional tree root value)',
+)
+StructureName = typer.Option(
+    gat.DEFAULT_STRUCTURE_NAME,
+    '-s',
+    '--structure',
+    help='structure mapping file (default: {gat.DEFAULT_STRUCTURE_NAME})',
+)
+TargetName = typer.Option(
+    '',
+    '-t',
+    '--target',
+    help='target document key',
+)
+FacetName = typer.Option(
+    '',
+    '-f',
+    '--facet',
+    help='facet key of target document',
+)
+Verbosity = typer.Option(
+    False,
+    '-v',
+    '--verbose',
+    help='Verbose output (default is False)',
+)
+Strictness = typer.Option(
+    False,
+    '-s',
+    '--strict',
+    help='Ouput noisy warnings on console (default is False)',
 )
 
 
@@ -42,70 +77,51 @@ def callback(
         raise typer.Exit()
 
 
-@app.command('verify')
-def verify(  # noqa
-    doc_root_pos: str = typer.Argument(''),
-    doc_root: str = typer.Option(
-        '',
-        '-d',
-        '--document-root',
-        help='Root of the document tree to visit. Optional\n(default: positional tree root value)',
-    ),
-    structure: str = typer.Option(
-        gat.DEFAULT_STRUCTURE_NAME,
-        '-s',
-        '--structure',
-        help='structure mapping file (default: {gat.DEFAULT_STRUCTURE_NAME})',
-    ),
-    target: str = typer.Option(
-        '',
-        '-t',
-        '--target',
-        help='target document key',
-    ),
-    facet: str = typer.Option(
-        '',
-        '-f',
-        '--facet',
-        help='facet key of target document',
-    ),
-    verbose: bool = typer.Option(
-        False,
-        '-v',
-        '--verbose',
-        help='Verbose output (default is False)',
-    ),
-    strict: bool = typer.Option(
-        False,
-        '-s',
-        '--strict',
-        help='Ouput noisy warnings on console (default is False)',
-    ),
-) -> int:
-    """
-    Verify the structure definition against the file system.
-    """
+def _verify_call_vector(
+    doc_root: str, doc_root_pos: str, verbose: bool, strict: bool
+) -> tuple[int, str, str, dict[str, bool]]:
+    """DRY"""
     doc = doc_root.strip()
     if not doc and doc_root_pos:
         doc = doc_root_pos
     if not doc:
         print('Document tree root required', file=sys.stderr)
-        return sys.exit(2)
+        return 2, 'Document tree root required', '', {}
 
     doc_root_path = pathlib.Path(doc)
     if doc_root_path.exists():
         if not doc_root_path.is_dir():
             print(f'requested tree root at ({doc}) is not a folder', file=sys.stderr)
-            return sys.exit(2)
+            return 2, f'requested tree root at ({doc}) is not a folder', '', {}
     else:
         print(f'requested tree root at ({doc}) does not exist', file=sys.stderr)
-        return sys.exit(2)
+        return 2, f'requested tree root at ({doc}) does not exist', '', {}
 
     options = {
         'quiet': QUIET and not verbose and not strict,
         'strict': strict,
         'verbose': verbose,
     }
+    return 0, '', doc, options
+
+
+@app.command('verify')
+def verify(  # noqa
+    doc_root_pos: str = typer.Argument(''),
+    doc_root: str = DocumentRoot,
+    structure: str = StructureName,
+    target: str = TargetName,
+    facet: str = FacetName,
+    verbose: bool = Verbosity,
+    strict: bool = Strictness,
+) -> int:
+    """
+    Verify the structure definition against the file system.
+    """
+    code, message, doc, options = _verify_call_vector(doc_root, doc_root_pos, verbose, strict)
+    if code:
+        log.error(message)
+        return 2
 
     return sys.exit(
         gat.verify(doc_root=doc, structure_name=structure, target_key=target, facet_key=facet, options=options)
@@ -115,67 +131,20 @@ def verify(  # noqa
 @app.command('approvals')
 def approvals(  # noqa
     doc_root_pos: str = typer.Argument(''),
-    doc_root: str = typer.Option(
-        '',
-        '-d',
-        '--document-root',
-        help='Root of the document tree to visit. Optional\n(default: positional tree root value)',
-    ),
-    structure: str = typer.Option(
-        gat.DEFAULT_STRUCTURE_NAME,
-        '-s',
-        '--structure',
-        help='structure mapping file (default: {gat.DEFAULT_STRUCTURE_NAME})',
-    ),
-    target: str = typer.Option(
-        '',
-        '-t',
-        '--target',
-        help='target document key',
-    ),
-    facet: str = typer.Option(
-        '',
-        '-f',
-        '--facet',
-        help='facet key of target document',
-    ),
-    verbose: bool = typer.Option(
-        False,
-        '-v',
-        '--verbose',
-        help='Verbose output (default is False)',
-    ),
-    strict: bool = typer.Option(
-        False,
-        '-s',
-        '--strict',
-        help='Ouput noisy warnings on console (default is False)',
-    ),
+    doc_root: str = DocumentRoot,
+    structure: str = StructureName,
+    target: str = TargetName,
+    facet: str = FacetName,
+    verbose: bool = Verbosity,
+    strict: bool = Strictness,
 ) -> int:
     """
     Weave in the approvals for facet of target within document root.
     """
-    doc = doc_root.strip()
-    if not doc and doc_root_pos:
-        doc = doc_root_pos
-    if not doc:
-        print('Document tree root required', file=sys.stderr)
-        return sys.exit(2)
-
-    doc_root_path = pathlib.Path(doc)
-    if doc_root_path.exists():
-        if not doc_root_path.is_dir():
-            print(f'requested tree root at ({doc}) is not a folder', file=sys.stderr)
-            return sys.exit(2)
-    else:
-        print(f'requested tree root at ({doc}) does not exist', file=sys.stderr)
-        return sys.exit(2)
-
-    options = {
-        'quiet': QUIET and not verbose and not strict,
-        'strict': strict,
-        'verbose': verbose,
-    }
+    code, message, doc, options = _verify_call_vector(doc_root, doc_root_pos, verbose, strict)
+    if code:
+        log.error(message)
+        return 2
 
     return sys.exit(
         sig.weave(doc_root=doc, structure_name=structure, target_key=target, facet_key=facet, options=options)
