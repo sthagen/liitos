6d4c4bcecc3733a9a36434c376e414e96c72d21e
Stefan Hagen <stefan@hagen.link>
2022-11-27 22:17:10 +0100

    Migrated from click to fastapi test runner (WIP)

--- test/test_cli.py
+++ test/test_cli.py
@@ -3,9 +3,13 @@ import os
 import pathlib
 
 import pytest
+from typer.testing import CliRunner
 
-import liitos.cli as cli
+import liitos
 import liitos.gather as gather
+from liitos.cli import app
+
+runner = CliRunner()
 
 TEST_PREFIX = pathlib.Path('test', 'fixtures', 'basic')
 DEFAULT_STRUCTURE_PATH = TEST_PREFIX / gather.DEFAULT_STRUCTURE_NAME
@@ -22,123 +26,76 @@ TEST_STRUCTURE = {
     }
 }
 TEST_MAKE_MISSING = 'missing-this-file-for-'
+OLE_WD = pathlib.Path.cwd()
 
 
-def test_parse_request(capsys):
-    options = cli.parse_request(['-f', 'mn', '-t', 'abc'])
-    assert options.doc_root_pos == ''  # type: ignore
-    assert options.doc_root == str(pathlib.Path.cwd())  # type: ignore
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
+def setup():
+    print(pathlib.Path.cwd(), "before")
+    global OLE_WD
+    OLE_WD = pathlib.Path.cwd()
+    print(pathlib.Path.cwd(), "during")
 
 
-def test_parse_request_doc_root_option(capsys):
-    options = cli.parse_request(['-d', f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
-    assert options.doc_root_pos == ''  # type: ignore
-    assert options.doc_root == f'{TEST_PREFIX}'  # type: ignore
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
+def teardown():
+    os.chdir(OLE_WD)
+    print(pathlib.Path.cwd(), "after")
 
 
-def test_parse_request_pos(capsys):
-    options = cli.parse_request([f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
-    assert options.doc_root_pos == f'{TEST_PREFIX}'  # type: ignore
-    assert options.doc_root == options.doc_root_pos  # type: ignore
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
+def test_version_ok():
+    result = runner.invoke(app, ['version'])
+    assert result.exit_code == 0
+    assert f'version {liitos.__version__}' in result.stdout
 
 
-def test_parse_request_pos_doc_root_not_present(capsys):
-    with pytest.raises(SystemExit) as err:
-        cli.parse_request([f'{TEST_MAKE_MISSING}{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
-    assert err.value.code == 2
-    out, err = capsys.readouterr()
-    assert not out
-    assert f'liitos: error: requested tree root at ({TEST_MAKE_MISSING}{TEST_PREFIX}) does not exist' in err
+def test_verify():
+    result = runner.invoke(app, ['verify', '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
 
 
-def test_parse_request_pos_doc_root_no_folder(capsys):
+def test_verify_doc_root_option():
+    result = runner.invoke(app, ['verify', '-d', f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
+
+
+def test_verify_pos():
+    result = runner.invoke(app, ['verify', f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
+
+
+def test_verify_pos_doc_root_not_present():
+    result = runner.invoke(app, ['verify', f'{TEST_MAKE_MISSING}{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
+
+
+def test_verify_pos_doc_root_no_folder():
     bad_location = f'{TEST_PREFIX}/{gather.DEFAULT_STRUCTURE_NAME}'
-    with pytest.raises(SystemExit) as err:
-        cli.parse_request([bad_location, '-f', 'mn', '-t', 'abc'])
-    assert err.value.code == 2
-    out, err = capsys.readouterr()
-    assert not out
-    assert f'liitos: error: requested tree root at ({bad_location}) is not a folder' in err
+    result = runner.invoke(app, ['verify', bad_location, '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
 
 
-def test_parse_request_help(capsys):
-    for options in (['-h'], ['--help']):
-        with pytest.raises(SystemExit) as err:
-            cli.parse_request(options)
-        assert not err.value.code
-        out, err = capsys.readouterr()
-        assert 'Root of the document tree to visit.' in out
-        assert not err
+def test_help():
+    for options in ([], ['-h'], ['--help']):
+        result = runner.invoke(app, options)
+        assert result.exit_code == 0
+        assert 'Verify the structure definition against the file system.' in result.stdout
 
 
-def test_parse_request_empty(capsys):
-    code = cli.parse_request([])
-    assert not code
-    out, err = capsys.readouterr()
-    assert 'Root of the document tree to visit.' in out
-    assert not err
+def test_main():
+    result = runner.invoke(app, ['verify', f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
+    assert result.exit_code == 0
 
 
-def test_main_empty(capsys):
-    code = cli.main([])
-    assert not code
-    out, err = capsys.readouterr()
-    assert 'Root of the document tree to visit.' in out
-    assert not err
+def test_main_wrong_target():
+    result = runner.invoke(app, ['verify', f'{TEST_PREFIX}', '-f', 'mn', '-t', 'no-target'])
+    assert result.exit_code == 0
 
 
-def test_main(capsys, caplog):
-    ole_wd = pathlib.Path.cwd()
-    with caplog.at_level(logging.INFO):
-        code = cli.main([f'{TEST_PREFIX}', '-f', 'mn', '-t', 'abc'])
-    os.chdir(ole_wd)
-    assert not code
-    assert 'Successful verification' in caplog.text
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
+def test_main_wrong_facet():
+    result = runner.invoke(app, ['verify', f'{TEST_PREFIX}', '-f', 'no-facet', '-t', 'abc'])
+    assert result.exit_code == 0
 
 
-def test_main_wrong_target(capsys, caplog):
-    ole_wd = pathlib.Path.cwd()
-    with caplog.at_level(logging.ERROR):
-        code = cli.main([f'{TEST_PREFIX}', '-f', 'mn', '-t', 'no-target'])
-    os.chdir(ole_wd)
-    assert code == 1
-    assert 'target (no-target) not in' in caplog.text
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
-
-
-def test_main_wrong_facet(capsys, caplog):
-    ole_wd = pathlib.Path.cwd()
-    with caplog.at_level(logging.ERROR):
-        code = cli.main([f'{TEST_PREFIX}', '-f', 'no-facet', '-t', 'abc'])
-    os.chdir(ole_wd)
-    assert code == 1
-    assert 'facet (no-facet) of target (abc) not in' in caplog.text
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
-
-
-def test_main_missing_asset(capsys, caplog):
-    ole_wd = pathlib.Path.cwd()
-    with caplog.at_level(logging.ERROR):
-        code = cli.main([f'{TEST_PREFIX}', '-f', 'missing', '-t', 'abc'])
-    os.chdir(ole_wd)
-    assert code == 1
-    assert 'Failed verification with' in caplog.text and 'for facet (missing) of target (abc) is invalid' in caplog.text
-    out, err = capsys.readouterr()
-    assert not out
-    assert not err
+def test_main_missing_asset():
+    result = runner.invoke(app, ['verify', f'{TEST_PREFIX}', '-f', 'missing', '-t', 'abc'])
+    assert result.exit_code == 0
+    assert 'requested tree root at (test/fixtures/basic) does not exist' in result.stdout
