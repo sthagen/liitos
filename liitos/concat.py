195c843f9921bc5bdc619e1e4be8a6d5d3b053af
Stefan Hagen <stefan@hagen.link>
2022-12-03 19:32:30 +0100

    Refactored CPSR blocks to prepare iterative implementation

--- liitos/concat.py
+++ liitos/concat.py
@@ -61,6 +61,72 @@ class RedirectedStdout:
         return self._string_io.getvalue()
 
 
+def adapt_image(text_line: str, collector: list[str], upstream: str, root: str) -> str:
+    """YES."""
+    before, xtr = text_line.split('](', 1)
+    has_caption = True if ' ' in xtr else False
+    img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
+    img_path = str((pathlib.Path(upstream).parent / img).resolve()).replace(root, '')
+    collector.append(img_path)
+    img_hack = img_path
+    if '/images/' in img_path:
+        img_hack = 'images/' + img_path.split('/images/', 1)[1]
+    elif '/diagrams/' in img_path:
+        img_hack = 'diagrams/' + img_path.split('/diagrams/', 1)[1]
+    if img_hack != img_path:
+        log.debug(f'{img_hack} <--- OK? --- {img_path}')
+    return f'{before}]({img_hack}{" " if has_caption else ")"}{after}'
+
+
+def harvest_include(
+    text_line: str,
+    slot: int,
+    regions: dict[str, list[tuple[tuple[int, int], str]]],
+    tree: treelib.Tree,
+    parent: str
+) -> None:
+    """TODO."""
+    include_local = text_line.split(INCLUDE_SLOT, 1)[1].rstrip('}').strip()
+    include = str(pathlib.Path(parent).parent / include_local)
+    regions[parent].append(((slot, slot), include))
+    tree.create_node(include, include, parent=parent)
+
+
+def rollup(
+    jobs: list[list[str]],
+    docs: dict[str, list[str]],
+    regions: dict[str, list[tuple[tuple[int, int], str]]],
+    flat: dict[str, str],
+) -> list[list[str]]:
+    """TODO."""
+    tackle = [those[0] for those in jobs if those]
+    if tackle:
+        log.info(f'  Insertion ongoing with parts ({", ".join(tuple(sorted(tackle)))}) remaining')
+    for that in tackle:
+        if that == '/':
+            continue
+        buf = []
+        for slot, line in enumerate(docs[that]):
+            special = False
+            the_first = False
+            the_include = ''
+            for pair, include in regions[that]:
+                low, high = pair
+                if low <= slot <= high:
+                    special = True
+                if low == slot:
+                    the_first = True
+                    the_include = include
+            if not special:
+                buf.append(line)
+                continue
+            if the_first:
+                buf.append(flat[the_include])
+        flat[that] = '\n'.join(buf) + '\n'
+
+    return [[job for job in chain if job not in flat] for chain in jobs]
+
+
 def concatenate(
     doc_root: str | pathlib.Path, structure_name: str, target_key: str, facet_key: str, options: dict[str, bool]
 ) -> int:
@@ -237,20 +303,7 @@ def concatenate(
             tree.create_node(entry, entry, parent=root)
             for slot, line in enumerate(documents[entry]):
                 if line.startswith(IMG_LINE_STARTSWITH):
-                    before, xtr = line.split('](', 1)
-                    has_caption = True if ' ' in xtr else False
-                    img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
-                    img_path = str((pathlib.Path(entry).parent / img).resolve()).replace(root_path, '')
-                    img_collector.append(img_path)
-                    img_hack = img_path
-                    if '/images/' in img_path:
-                        img_hack = 'images/' + img_path.split('/images/', 1)[1]
-                    elif '/diagrams/' in img_path:
-                        img_hack = 'diagrams/' + img_path.split('/diagrams/', 1)[1]
-                    if img_hack != img_path:
-                        log.debug(f'{img_hack} <--- OK? --- {img_path}')
-                    line = f'{before}]({img_hack}{" " if has_caption else ")"}{after}'
-                    documents[entry][slot] = line
+                    documents[entry][slot] = adapt_image(line, img_collector, entry, root_path)
                 log.debug(f'{slot :02d}|{line.rstrip()}')
                 if not in_region:
                     if line.startswith(READ_SLOT_FENCE_BEGIN):
@@ -284,13 +337,7 @@ def concatenate(
                 sub_include = ''
                 for slot, line in enumerate(documents[include]):
                     if line.startswith(IMG_LINE_STARTSWITH):
-                        before, xtr = line.split('](', 1)
-                        has_caption = True if ' ' in xtr else False
-                        img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
-                        img_path = str((pathlib.Path(include).parent / img).resolve()).replace(root_path, '')
-                        img_collector.append(img_path)
-                        line = f'{before}]({img}{" " if has_caption else ")"}{after}'
-                        documents[include][slot] = line
+                        documents[include][slot] = adapt_image(line, img_collector, include, root_path)
                     log.debug(f'{slot :02d}|{line.rstrip()}')
                     if not in_region:
                         if line.startswith(READ_SLOT_FENCE_BEGIN):
@@ -298,11 +345,7 @@ def concatenate(
                             begin = slot
                             continue
                         if line.startswith(INCLUDE_SLOT):
-                            sub_include = line.split(INCLUDE_SLOT, 1)[1].rstrip('}').strip()
-                            sub_include = str(pathlib.Path(include).parent / sub_include)
-                            insert_regions[include].append(((slot, slot), sub_include))
-                            tree.create_node(sub_include, sub_include, parent=include)
-                            sub_include = ''
+                            harvest_include(line, slot, insert_regions, tree, include)
                             continue
                     if in_region:
                         if line.startswith(READ_SLOT_CONTEXT_BEGIN):
@@ -326,13 +369,7 @@ def concatenate(
                     sub_sub_include = ''
                     for slot, line in enumerate(documents[sub_include]):
                         if line.startswith(IMG_LINE_STARTSWITH):
-                            before, xtr = line.split('](', 1)
-                            has_caption = True if ' ' in xtr else False
-                            img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
-                            img_path = str((pathlib.Path(sub_include).parent / img).resolve()).replace(root_path, '')
-                            img_collector.append(img_path)
-                            line = f'{before}]({img}{" " if has_caption else ")"}{after}'
-                            documents[sub_include][slot] = line
+                            documents[sub_include][slot] = adapt_image(line, img_collector, sub_include, root_path)
                         log.debug(f'{slot :02d}|{line.rstrip()}')
                         if not in_region:
                             if line.startswith(READ_SLOT_FENCE_BEGIN):
@@ -340,11 +377,7 @@ def concatenate(
                                 begin = slot
                                 continue
                             if line.startswith(INCLUDE_SLOT):
-                                sub_sub_include = line.split(INCLUDE_SLOT, 1)[1].rstrip('}').strip()
-                                sub_sub_include = str(pathlib.Path(sub_include).parent / sub_sub_include)
-                                insert_regions[sub_include].append(((slot, slot), sub_sub_include))
-                                tree.create_node(sub_sub_include, sub_sub_include, parent=sub_include)
-                                sub_sub_include = ''
+                                harvest_include(line, slot, insert_regions, tree, sub_include)
                                 continue
                         if in_region:
                             if line.startswith(READ_SLOT_CONTEXT_BEGIN):
@@ -368,13 +401,7 @@ def concatenate(
                         sub_sub_sub_include = ''
                         for slot, line in enumerate(documents[sub_sub_include]):
                             if line.startswith(IMG_LINE_STARTSWITH):
-                                before, xtr = line.split('](', 1)
-                                has_caption = True if ' ' in xtr else False
-                                img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
-                                img_path = str((pathlib.Path(sub_sub_include).parent / img).resolve()).replace(root_path, '')
-                                img_collector.append(img_path)
-                                line = f'{before}]({img}{" " if has_caption else ")"}{after}'
-                                documents[sub_sub_include][slot] = line
+                                documents[sub_sub_include][slot] = adapt_image(line, img_collector, sub_sub_include, root_path)
                             log.debug(f'{slot :02d}|{line.rstrip()}')
                             if not in_region:
                                 if line.startswith(READ_SLOT_FENCE_BEGIN):
@@ -382,11 +409,7 @@ def concatenate(
                                     begin = slot
                                     continue
                                 if line.startswith(INCLUDE_SLOT):
-                                    sub_sub_sub_include = line.split(INCLUDE_SLOT, 1)[1].rstrip('}').strip()
-                                    sub_sub_sub_include = str(pathlib.Path(sub_sub_include).parent / sub_sub_sub_include)
-                                    insert_regions[sub_sub_include].append(((slot, slot), sub_sub_sub_include))
-                                    tree.create_node(sub_sub_sub_include, sub_sub_sub_include, parent=sub_sub_include)
-                                    sub_sub_sub_include = ''
+                                    harvest_include(line, slot, insert_regions, tree, sub_sub_include)
                                     continue
                             if in_region:
                                 if line.startswith(READ_SLOT_CONTEXT_BEGIN):
@@ -410,13 +433,9 @@ def concatenate(
                             sub_sub_sub_sub_include = ''
                             for slot, line in enumerate(documents[sub_sub_sub_include]):
                                 if line.startswith(IMG_LINE_STARTSWITH):
-                                    before, xtr = line.split('](', 1)
-                                    has_caption = True if ' ' in xtr else False
-                                    img, after = xtr.split(' ', 1) if has_caption else xtr.split(')', 1)
-                                    img_path = str((pathlib.Path(sub_sub_sub_include).parent / img).resolve()).replace(root_path, '')
-                                    img_collector.append(img_path)
-                                    line = f'{before}]({img}{" " if has_caption else ")"}{after}'
-                                    documents[sub_sub_sub_include][slot] = line
+                                    documents[sub_sub_sub_include][slot] = adapt_image(
+                                        line, img_collector, sub_sub_sub_include, root_path
+                                    )
                                 log.debug(f'{slot :02d}|{line.rstrip()}')
                                 if not in_region:
                                     if line.startswith(READ_SLOT_FENCE_BEGIN):
@@ -424,13 +443,7 @@ def concatenate(
                                         begin = slot
                                         continue
                                     if line.startswith(INCLUDE_SLOT):
-                                        sub_sub_sub_sub_include = line.split(INCLUDE_SLOT, 1)[1].rstrip('}').strip()
-                                        sub_sub_sub_sub_include = str(
-                                            pathlib.Path(sub_sub_sub_include).parent / sub_sub_sub_sub_include
-                                        )
-                                        insert_regions[sub_sub_sub_sub_include].append(((slot, slot), sub_sub_sub_sub_include))
-                                        tree.create_node(sub_sub_sub_sub_include, sub_sub_sub_sub_include, parent=sub_sub_sub_include)
-                                        sub_sub_sub_sub_include = ''
+                                        harvest_include(line, slot, insert_regions, tree, sub_sub_sub_include)
                                         continue
                                 if in_region:
                                     if line.startswith(READ_SLOT_CONTEXT_BEGIN):
@@ -475,87 +488,10 @@ def concatenate(
 
         chains = [leaf_path for leaf_path in bottom_up_paths]
         log.info(f'starting insertions bottom up for the {len(chains)} inclusion chains:')
-
         remaining = [[job for job in chain if job not in concat] for chain in chains]
-        tackle = [those[0] for those in remaining if those]
-        if tackle:
-            log.info(f'  Insertion ongoing with parts ({", ".join(tuple(sorted(tackle)))}) remaining')
-        for that in tackle:
-            if that == '/':
-                continue
-            buf = []
-            for slot, line in enumerate(documents[that]):
-                special = False
-                the_first = False
-                the_include = ''
-                for pair, include in insert_regions[that]:
-                    low, high = pair
-                    if low <= slot <= high:
-                        special = True
-                    if low == slot:
-                        the_first = True
-                        the_include = include
-                if not special:
-                    buf.append(line)
-                    continue
-                if the_first:
-                    buf.append(concat[the_include])
-            concat[that] = '\n'.join(buf) + '\n'
-
-        still = [[job for job in chain if job not in concat] for chain in remaining]
-        tackle = set(those[0] for those in still if those)
-        if tackle:
-            log.info(f'  Insertion ongoing with parts ({", ".join(tuple(sorted(tackle)))}) remaining')
-        for that in tackle:
-            if that == '/':
-                continue
-            buf = []
-            for slot, line in enumerate(documents[that]):
-                special = False
-                the_first = False
-                the_include = ''
-                for pair, include in insert_regions[that]:
-                    low, high = pair
-                    if low <= slot <= high:
-                        special = True
-                    if low == slot:
-                        the_first = True
-                        the_include = include
-                if not special:
-                    buf.append(line)
-                    continue
-                if the_first:
-                    buf.append(concat[the_include])
-            concat[that] = '\n'.join(buf) + '\n'
-
-        more = [[job for job in chain if job not in concat] for chain in still]
-        tackle = set(those[0] for those in more if those)
-        if tackle:
-            log.info(f'  Insertion ongoing with parts ({", ".join(tuple(sorted(tackle)))}) remaining')
-        for that in tackle:
-            if that == '/':
-                continue
-            # PROTOBUG print(that, insert_regions[that])
-            buf = []
-            for slot, line in enumerate(documents[that]):
-                special = False
-                the_first = False
-                the_include = ''
-                for pair, include in insert_regions[that]:
-                    low, high = pair
-                    if low <= slot <= high:
-                        special = True
-                    if low == slot:
-                        the_first = True
-                        the_include = include
-                if not special:
-                    buf.append(line)
-                    continue
-                if the_first:
-                    buf.append(concat[the_include])
-            concat[that] = '\n'.join(buf) + '\n'
-
-        done = [[job for job in chain if job not in concat] for chain in more]
+        still = rollup(remaining, documents, insert_regions, concat)
+        more = rollup(still, documents, insert_regions, concat)
+        done = rollup(more, documents, insert_regions, concat)
         tackle = set(those[0] for those in done if those)
         if tackle and len(tackle) >= 1 and tuple(tackle)[0] != '/':
             log.warning(f' Insertion incomplete with parts ({", ".join(tuple(sorted(tackle)))}) remaining')
