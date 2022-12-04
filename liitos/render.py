47247d129f126b57c6444478284630af11f7eb04
Stefan Hagen <stefan@hagen.link>
2022-12-04 16:59:26 +0100

    More plumbing towards final PDF rendering

--- liitos/render.py
+++ liitos/render.py
@@ -5,6 +5,9 @@ temp_md="_patched.md"
 sed "s/\.drawio\.svg/.png/g;" < document.md > "${temp_md}" && mv "${temp_md}" document.md
 sed "s/\.svg/.png/g;" < document.md > "${temp_md}" && mv "${temp_md}" document.md
 pandoc -f markdown+link_attributes -t latex document.md -o document.tex --filter mermaid-filter
+./captions-below < document.tex > captions-below.tex
+./inject-stem-label < document.tex > injected-stem-labels.tex
+./scale-figures < document.tex > scaled-figures.tex
 cp -a driver.tex this.tex
 lualatex --shell-escape this.tex
 lualatex --shell-escape this.tex
@@ -12,6 +15,7 @@ lualatex --shell-escape this.tex
 """
 import os
 import pathlib
+import shutil
 import subprocess
 
 
@@ -22,6 +26,9 @@ from liitos import ENCODING, log
 
 DOC_BASE = pathlib.Path('..', '..')
 STRUCTURE_PATH = DOC_BASE / 'structure.yml'
+IMAGES_FOLDER = 'images/'
+DIAGRAMS_FOLDER = 'diagrams/'
+
 
 def der(
     doc_root: str | pathlib.Path, structure_name: str, target_key: str, facet_key: str, options: dict[str, bool]
@@ -96,21 +103,95 @@ def der(
             return 0
 
         log.info('transforming SVG assets to high resolution PNG bitmaps ...')
-        try:
-            retcode = subprocess.call("ls -lrt", shell=True)
-            if retcode < 0:
-                log.error(f'svg-to-png process was terminated by signal {-retcode}')
-            else:
-                log.info(f'svg-to-png process returned {retcode}')
-        except OSError as err:
-            print(f'execution of svg-to-png process failed: {err}')
+        for path_to_dir in (IMAGES_FOLDER, DIAGRAMS_FOLDER):
+            for svg in pathlib.Path(path_to_dir).iterdir():
+                if svg.is_file() and svg.suffix == '.svg':
+                    png = str(svg).replace('.svg', '.png')
+                    command = f'svgexport "{svg}" "{png}" 100%'
+                    try:
+                        return_code = subprocess.call(command, shell=True)
+                        if return_code < 0:
+                            log.error(f'svg-to-png process ({command}) was terminated by signal {-return_code}')
+                        else:
+                            log.info(f'svg-to-png process ({command}) returned {return_code}')
+                    except OSError as err:
+                        print(f'execution of svg-to-png process ({command}) failed: {err}')
 
         log.info('rewriting src attribute values of SVG to PNG sources ...')
+        with open('document.md', 'rt', encoding=ENCODING) as handle:
+            lines = [line.rstrip() for line in handle.readlines()]
+        for slot, line in enumerate(lines):
+            if '.drawio.svg' in line:
+                lines[slot] = line.replace('.drawio.svg', '.png')
+                continue
+            if '.svg' in line:
+                lines[slot] = line.replace('.svg', '.png')
+                continue
+        with open('document.md', 'wt', encoding=ENCODING) as handle:
+            handle.write('\n'.join(lines))
+
         log.info('pandoc -f markdown+link_attributes -t latex document.md -o document.tex --filter mermaid-filter ...')
+        command = 'pandoc -f markdown+link_attributes -t latex document.md -o document.tex --filter mermaid-filter'
+        try:
+            return_code = subprocess.call(command, shell=True)
+            if return_code < 0:
+                log.error(f'md-to-tex process ({command}) was terminated by signal {-return_code}')
+            else:
+                log.info(f'md-to-tex process ({command}) returned {return_code}')
+        except OSError as err:
+            print(f'execution of md-to-tex process ({command}) failed: {err}')
+
+        log.info('./captions-below < document.tex > captions-below.tex ...')
+        log.info('./inject-stem-label < document.tex > injected-stem-labels.tex ...')
+        log.info('./scale-figures < document.tex > scaled-figures.tex ...')
+
         log.info('cp -a driver.tex this.tex ...')
+        source_asset = 'driver.tex'
+        target_asset = 'this.tex'
+        shutil.copy(source_asset, target_asset)
+
+
+        def log_subprocess_output(pipe, prefix: str):
+            for line in iter(pipe.readline, b''):  # b'\n'-separated lines
+                log.info(f'{prefix}: %r', line)
+
+
+        latex_to_pdf_command = ['lualatex', '--shell-escape', 'this.tex']
         log.info('1/3) lualatex --shell-escape this.tex ...')
+        process = subprocess.Popen(latex_to_pdf_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        with process.stdout:
+            log_subprocess_output(process.stdout, 'latex-to-pdf(1/3)')
+        return_code = process.wait()  # 0 means success
+        if return_code < 0:
+            log.error(f'latex-to-pdf process 1/3 ({latex_to_pdf_command}) was terminated by signal {-return_code}')
+        else:
+            log.info(f'latex-to-pdf process 1/3  ({latex_to_pdf_command}) returned {return_code}')
+
         log.info('2/3) lualatex --shell-escape this.tex ...')
+        process = subprocess.Popen(latex_to_pdf_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        with process.stdout:
+            log_subprocess_output(process.stdout, 'latex-to-pdf(2/3)')
+        return_code = process.wait()  # 0 means success
+        if return_code < 0:
+            log.error(f'latex-to-pdf process 2/3 ({latex_to_pdf_command}) was terminated by signal {-return_code}')
+        else:
+            log.info(f'latex-to-pdf process 2/3  ({latex_to_pdf_command}) returned {return_code}')
+
         log.info('3/3) lualatex --shell-escape this.tex ...')
+        process = subprocess.Popen(latex_to_pdf_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        with process.stdout:
+            log_subprocess_output(process.stdout, 'latex-to-pdf(3/3)')
+        return_code = process.wait()  # 0 means success
+        if return_code < 0:
+            log.error(f'latex-to-pdf process 3/3 ({latex_to_pdf_command}) was terminated by signal {-return_code}')
+        else:
+            log.info(f'latex-to-pdf process 3/3  ({latex_to_pdf_command}) returned {return_code}')
+
         log.info('Moving stuff around (result phase) ...')
+        source_asset = 'this.pdf'
+        target_asset = '../index.pdf'
+        shutil.copy(source_asset, target_asset)
+
+        log.info('done.')
 
     return 0
