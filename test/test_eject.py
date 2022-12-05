0b5834d1ab36fb7f9d4d83ee1703dc705fd5b7fe
Stefan Hagen <stefan@hagen.link>
2022-12-05 16:16:39 +0100

    Added tests for ejecting templates

---
+++ test/test_eject.py
@@ -0,0 +1,18 @@
+import yaml
+
+import liitos.eject as eject
+from liitos.meta import MAGIC_OF_TODAY
+
+
+def test_this_ok(capsys):
+    assert eject.this('meta-patch-yaml') == 0
+    out, err = capsys.readouterr()
+    assert not err
+    assert 'patch' in out
+    interpret_as_yaml = yaml.safe_load(out)
+    assert interpret_as_yaml['document']['patch']['header_date'] == MAGIC_OF_TODAY
+
+
+def test_this_wrong_thing(caplog):
+    assert eject.this('unknown-thing') == 2
+    assert 'unknown-thing' in caplog.text
