e3d851f48689e36d250c65b1409ab997f76f04e8
Stefan Hagen <stefan@hagen.link>
2022-12-05 17:36:18 +0100

    Extended labels tests

--- test/test_labels.py
+++ test/test_labels.py
@@ -8,3 +8,23 @@ def test_inject_on_empty_ok():
 def test_inject_no_match_passes_through_ok():
     pass_me_through = ['a', 'b', 'caption', 'figure', 'table', 'label']
     assert labels.inject(pass_me_through) == pass_me_through
+
+
+def test_inject_precondition():
+    partial_matches = ['a', r'\begin{figure}', 'z']
+    assert labels.inject(partial_matches) == partial_matches
+
+
+def test_inject_not_precondition():
+    partial_matches = ['a', r'\includegraphics{', 'z']
+    augmented_content = [
+        'a',
+        '',
+        r'\begin{figure}',
+        r'\centering',
+        r'\includegraphics{',
+        r'\caption{MISSING-CAPTION-IN-MARKDOWN FIX-AT-SOURCE}',
+        r'\end{figure}',
+        'z',
+    ]
+    assert labels.inject(partial_matches) == augmented_content
