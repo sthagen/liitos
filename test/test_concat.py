8d1499190915930e6cc83b6afde83259df3585c3
Stefan Hagen <stefan@hagen.link>
2022-12-06 21:24:22 +0100

    Added real world concat tests

--- test/test_concat.py
+++ test/test_concat.py
@@ -1,6 +1,9 @@
+import os
 import pathlib
 
 import liitos.concat as concat
+
+BASIC_FIXTURE_ROOT = pathlib.Path('test', 'fixtures', 'basic')
 
 
 def test_adapt_image_images():
@@ -20,3 +23,15 @@ def test_adapt_image_other():
     # This may be not what anyone wants ...
     assert concat.adapt_image('](x/other/abc.def)', collector, 'x', root='y') == f']({pathlib.Path().cwd()}/x/other/abc.def)'
     assert collector == [f'{pathlib.Path().cwd()}/x/other/abc.def']
+
+def test_concatenate():
+    parameters = {
+        'doc_root': BASIC_FIXTURE_ROOT,
+        'structure_name': 'structure.yml',
+        'target_key': 'abc',
+        'facet_key': 'mn',
+        'options': {},
+    }
+    restore = os.getcwd()
+    assert concat.concatenate(**parameters) == 0
+    os.chdir(restore)
