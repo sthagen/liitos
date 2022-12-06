6b7e094c80139cd437dbf79ea492318d1045aa32
Stefan Hagen <stefan@hagen.link>
2022-12-06 20:57:06 +0100

    Added table tests

---
+++ test/test_tables.py
@@ -0,0 +1,18 @@
+import liitos.tables as tables
+
+def test_patch_empty():
+    assert tables.patch([]) == []
+
+
+def test_patch_tab_start():
+    incoming = [
+        '',
+        tables.TAB_START_TOK,
+        tables.TOP_RULE,
+        tables.MID_RULE,
+        tables.END_HEAD,
+        tables.END_DATA_ROW,
+        tables.BOT_RULE,
+        tables.TAB_END_TOK,
+    ]
+    assert tables.patch(incoming) == incoming
