21b7ee0ef05ed4e51f3e64df8f17485650fced62
Stefan Hagen <stefan@hagen.link>
2022-12-06 22:21:15 +0100

    Bumped impl version fro release 2022.12.6

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -6,8 +6,8 @@ import pathlib
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2022.12.5+parent.aec13b7a'
-# [[[end]]] (checksum: 11837d6656c253a0dc618355c790bd3e)
+__version__ = '2022.12.6+parent.e68424eb'
+# [[[end]]] (checksum: 42b491a202e452d0d220e3c599c4d341)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
