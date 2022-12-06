c4f4fb4353f833edae8dd1a55da1e5d34e43676f
Stefan Hagen <stefan@hagen.link>
2022-12-06 21:39:03 +0100

    Added another real world test to concat suite

--- test/test_concat.py
+++ test/test_concat.py
@@ -4,6 +4,7 @@ import pathlib
 import liitos.concat as concat
 
 BASIC_FIXTURE_ROOT = pathlib.Path('test', 'fixtures', 'basic')
+EXAMPLE_DEEP_DOC_ROOT = pathlib.Path('example', 'deep')
 
 
 def test_adapt_image_images():
@@ -24,12 +25,25 @@ def test_adapt_image_other():
     assert concat.adapt_image('](x/other/abc.def)', collector, 'x', root='y') == f']({pathlib.Path().cwd()}/x/other/abc.def)'
     assert collector == [f'{pathlib.Path().cwd()}/x/other/abc.def']
 
-def test_concatenate():
+def test_concatenate_base():
     parameters = {
         'doc_root': BASIC_FIXTURE_ROOT,
         'structure_name': 'structure.yml',
         'target_key': 'abc',
         'facet_key': 'mn',
+        'options': {},
+    }
+    restore = os.getcwd()
+    assert concat.concatenate(**parameters) == 0
+    os.chdir(restore)
+
+
+def test_concatenate_deep():
+    parameters = {
+        'doc_root': EXAMPLE_DEEP_DOC_ROOT,
+        'structure_name': 'structure.yml',
+        'target_key': 'prod_kind',
+        'facet_key': 'deep',
         'options': {},
     }
     restore = os.getcwd()
