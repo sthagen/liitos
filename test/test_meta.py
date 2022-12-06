af38ac02f6ccacadda0c90aa3609713f4476a90b
Stefan Hagen <stefan@hagen.link>
2022-12-06 15:44:40 +0100

    Added another simple test for meta

--- test/test_meta.py
+++ test/test_meta.py
@@ -3,3 +3,7 @@ import liitos.meta as meta
 
 def test_weave_meta_part_proprietary_information_on_empty_ok():
     assert meta.weave_meta_part_proprietary_information({}, '') == ''
+
+
+def test_weave_meta_part_proprietary_information_on_value_slot_ok():
+    assert meta.weave_meta_part_proprietary_information({}, '-VALUE.SLOT+') == '-Proprietary Information MISSING+'
