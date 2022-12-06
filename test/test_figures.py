1674ea735a43fc574253f7722ea8ae9af8f99630
Stefan Hagen <stefan@hagen.link>
2022-12-06 20:41:24 +0100

    Tests, tests, tests

--- test/test_figures.py
+++ test/test_figures.py
@@ -19,6 +19,11 @@ def test_scale_trigger_complete():
     assert figures.scale(incoming) == ['', '']  # This is may be surprising: error yes, but silently steal?
 
 
+def test_scale_trigger_decimal_complete():
+    incoming = ['', r'\scale=0.9', '']
+    assert figures.scale(incoming) == ['', '']  # This is may be surprising: error yes, but silently steal?
+
+
 def test_scale_trigger_complete_plus():
     incoming = ['', r'\scale=90%', '', r'\includegraphics{']
     assert figures.scale(incoming) == ['', '', r'\includegraphics{']
@@ -31,5 +36,17 @@ def test_scale_copy_through():
 
 def test_scale_trigger_percent_complete_plus():
     incoming = ['', r'\scale=90\%', '', 'text', r'\includegraphics{', '', 'x']
+    outgoing = ['', '', 'text', r'\includegraphics[width=0.9\textwidth,height=0.9\textheight]{','', 'x']
+    assert figures.scale(incoming) == outgoing
+
+
+def test_scale_trigger_percent_tiny__complete_plus():
+    incoming = ['', r'\scale=0.90000000000000000001\%', '', 'text', r'\includegraphics{', '', 'x']
+    outgoing = ['', '', 'text', r'\includegraphics[width=0.01\textwidth,height=0.01\textheight]{','', 'x']
+    assert figures.scale(incoming) == outgoing
+
+
+def test_scale_trigger_fraction_complete_plus():
+    incoming = ['', r'\scale=0.90000000000000000001', '', 'text', r'\includegraphics{', '', 'x']
     outgoing = ['', '', 'text', r'\includegraphics[width=0.9\textwidth,height=0.9\textheight]{','', 'x']
     assert figures.scale(incoming) == outgoing
