84b41a76c10dc7c43437bc7638486e8098c90190
Stefan Hagen <stefan@hagen.link>
2022-12-06 20:01:06 +0100

    Added some more figures tests

--- test/test_figures.py
+++ test/test_figures.py
@@ -3,3 +3,22 @@ import liitos.figures as figures
 
 def test_scale_empty():
     assert figures.scale([]) == []
+
+
+def test_scale_trigger_but_incomplete():
+    assert figures.scale([r'\scale=']) == []  # This is may be surprising: error yes, but silently steal?
+
+
+def test_scale_trigger_plus():
+    incoming = [r'\scale=', r'\includegraphics{']
+    assert figures.scale(incoming) == [r'\includegraphics{']  # This is may be surprising: error yes, but silently steal?
+
+
+def test_scale_trigger_complete():
+    incoming = ['', r'\scale=90%', '']
+    assert figures.scale(incoming) == ['', '']  # This is may be surprising: error yes, but silently steal?
+
+
+def test_scale_trigger_complete_plus():
+    incoming = ['', r'\scale=90%', '', r'\includegraphics{']
+    assert figures.scale(incoming) == ['', '', r'\includegraphics{']
