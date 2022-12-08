542883c6f084efd4c1a47213f5e0b30c501107fe
Stefan Hagen <stefan@hagen.link>
2022-12-08 18:53:59 +0100

    Added another tables test to prepare real implementation work

--- test/test_tables.py
+++ test/test_tables.py
@@ -1,5 +1,26 @@
 import liitos.tables as tables
 
+TABLE_THREE_COLS = r"""\
+\begin{longtable}[]{@{}lcr@{}}
+\caption{A caption for a table
+\label{table:left-middle-right}}\tabularnewline
+\toprule()
+Left & Middle & Right \\
+\midrule()
+\endfirsthead
+\toprule()
+Left & Middle & Right \\
+\midrule()
+\endhead
+L1 & M2 & R3 \\
+L4 & M5 & R6 \\
+L7 & M8 & R9 \\
+L10 & M11 & R12 \\
+L13 & M14 & R15 \\
+\bottomrule()
+\end{longtable}
+"""
+
 
 def test_patch_empty():
     assert tables.patch([]) == []
@@ -16,4 +37,8 @@ def test_patch_tab_start():
         tables.BOT_RULE,
         tables.TAB_END_TOK,
     ]
+    assert tables.patch(incoming) == incoming
+
+def test_patch_tab_three_cols():
+    incoming = TABLE_THREE_COLS.split()
     assert tables.patch(incoming) == incoming
