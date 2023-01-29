8873c035b7c7e852d6eda748e485c4756a937bdb
Stefan Hagen <stefan@hagen.link>
2023-01-29 19:13:05 +0100

    Refactored option command parsing and extended logged information

--- liitos/description_lists.py
+++ liitos/description_lists.py
@@ -5,6 +5,22 @@ from liitos import log
 NO_OPTION: str = ''
 
 
+def parse_options_command(slot: int, text_line: str) -> tuple[bool, str, str]:
+    """Parse the \\option[style=multiline,leftmargin=6em]."""
+    if text_line.startswith(r'\option['):
+        log.info(f'trigger an option mod for the next description environment at line #{slot + 1}|{text_line}')
+        try:
+            # \option[style=multiline,leftmargin=6em]  --> [style=multiline,leftmargin=6em]
+            opt = text_line.split(r'\option', 1)[1].strip()
+            log.info(f' -> parsed option as ({opt})')
+            return True, f'%CONSIDERED_{text_line}', opt
+        except Exception as err:
+            log.error(f'failed to parse option value from {text_line.strip()} with err: {err}')
+            return False, text_line, ''
+    else:
+        return False, text_line, ''
+
+
 def options(incoming: Iterable[str]) -> list[str]:
     """Later alligator. \\option[style=multiline,leftmargin=6em]"""
     outgoing = []
@@ -12,28 +28,23 @@ def options(incoming: Iterable[str]) -> list[str]:
     opt = NO_OPTION
     for slot, line in enumerate(incoming):
         if modus == 'copy':
-            if line.startswith(r'\option['):
-                log.info(f'trigger an option mod for the next description environment at line #{slot + 1}|{line}')
-                modus = 'scale'
-                option_save = line  # only for reporting will not pass the filter
-                try:
-                    # \option[style=multiline,leftmargin=6em]  --> [style=multiline,leftmargin=6em]
-                    opt = option_save.split(r'\option', 1)[1].strip()
-                except Exception as err:
-                    log.error(f'failed to parse option value from {line.strip()} with err: {err}')
+            has_opt, text_line, opt = parse_options_command(slot, line)
+            if has_opt:
+                modus = 'option'
             else:
-                outgoing.append(line)
+                outgoing.append(text_line)
+            continue
 
-        else:  # if modus == 'scale':
-            if line.startswith(r'\begin{description}'):
-                if opt != NO_OPTION:
-                    log.info(f'- found the option target start at line #{slot + 1}|{line}')
-                    outgoing.append(f'\\begin{{description}}{opt}')
-                else:
-                    outgoing.append(line)
-                modus = 'copy'
-                opt = NO_OPTION
+        # if modus == 'option':
+        if line.startswith(r'\begin{description}'):
+            if opt != NO_OPTION:
+                log.info(f'- found the option target start at line #{slot + 1}|{line}')
+                outgoing.append(f'\\begin{{description}}{opt}')
             else:
                 outgoing.append(line)
+            modus = 'copy'
+            opt = NO_OPTION
+        else:
+            outgoing.append(line)
 
     return outgoing
