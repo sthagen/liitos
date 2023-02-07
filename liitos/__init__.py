55300827e54d4eb61fe80dc0bd5a9d318a43e3b0
Stefan Hagen <stefan@hagen.link>
2023-02-07 23:59:24 +0100

    Bumped impl version for release 2023.2.8

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -7,8 +7,8 @@ import shellingham  # type: ignore
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2023.2.7+parent.4f01d7b3'
-# [[[end]]] (checksum: 532593e3f30a4a904e6ed4486e271f7e)
+__version__ = '2023.2.8+parent.b08058c7'
+# [[[end]]] (checksum: 66a50e360bdddcd6850848c3808a189f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
