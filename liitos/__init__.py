a29e48d098a3079801e2cc4abaf5326dfad82194
Stefan Hagen <stefan@hagen.link>
2023-07-01 18:36:10 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -13,18 +13,19 @@ __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
 
-APP_NAME = 'Splice (Finnish liitos) contributions.'
-APP_ALIAS = 'liitos'
-APP_ENV = 'LIITOS'
-APP_VERSION = __version__
-COMMA = ','
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
+APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
 DEBUG = bool(os.getenv(f'{APP_ENV}_DEBUG', ''))
 VERBOSE = bool(os.getenv(f'{APP_ENV}_VERBOSE', ''))
 QUIET = False
 STRICT = bool(os.getenv(f'{APP_ENV}_STRICT', ''))
 ENCODING = 'utf-8'
 ENCODING_ERRORS_POLICY = 'ignore'
-DEFAULT_CONFIG_NAME = '.liitos.json'
+DEFAULT_CONFIG_NAME = f'.{APP_ALIAS}.json'
+
+APP_VERSION = __version__
+COMMA = ','
 DEFAULT_LF_ONLY = 'YES'
 FILTER_CS_LIST = 'mermaid-filter'
 FROM_FORMAT_SPEC = 'markdown'
