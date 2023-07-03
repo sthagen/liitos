0fa36821bede7b9a9bd1a86cb99991ed67f9af8e
Stefan Hagen <stefan@hagen.link>
2023-07-03 23:35:42 +0200

    Bumped impl version to test the new source topology

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -7,8 +7,8 @@ import shellingham  # type: ignore
 from typing import List, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2023.6.25+parent.ga2a8921e'
-# [[[end]]] (checksum: afe822f28ae8e3e33a4052e9cff5789a)
+__version__ = '2023.6.25+parent.gaee49bb0'
+# [[[end]]] (checksum: 6c7154918ed8d6742eb804487e761fa8)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
