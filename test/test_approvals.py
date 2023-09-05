2c6d3cedbeb98af545edf90b10e13654d62738a1
Stefan Hagen <stefan@hagen.link>
2023-09-05 19:21:51 +0200

    Added legacy approvals weave test

--- test/test_approvals.py
+++ test/test_approvals.py
@@ -5,6 +5,7 @@ import liitos.approvals as approvals
 
 BASIC_FIXTURE_ROOT = pathlib.Path('test', 'fixtures', 'basic')
 EXAMPLE_DEEP_DOC_ROOT = pathlib.Path('example', 'deep')
+EXAMPLE_LEGACY_DOC_ROOT = pathlib.Path('example', 'legacy')
 
 
 def test_approvals():
@@ -13,6 +14,19 @@ def test_approvals():
         'structure_name': 'structure.yml',
         'target_key': 'prod_kind',
         'facet_key': 'deep',
+        'options': {},
+    }
+    restore = os.getcwd()
+    assert approvals.weave(**parameters) == 0
+    os.chdir(restore)
+
+
+def test_approvals_legacy():
+    parameters = {
+        'doc_root': EXAMPLE_LEGACY_DOC_ROOT,
+        'structure_name': 'structure.yml',
+        'target_key': 'prod_kind',
+        'facet_key': 'legacy',
         'options': {},
     }
     restore = os.getcwd()
