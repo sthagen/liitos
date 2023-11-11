94a809f8738b011d8b4e684f00c7cddb6e199030
Stefan Hagen <stefan@hagen.link>
2023-11-11 18:17:30 +0100

    Fixed bug in label injector where figure parsing leaves out log points #48 (https://todo.sr.ht/~sthagen/liitos/48)

--- liitos/labels.py
+++ liitos/labels.py
@@ -6,6 +6,18 @@ from liitos import log
 NO_LABEL = 'no-label-found-ERROR'
 
 
+def is_include_graphics(text: str) -> bool:
+    """Only DRY."""
+    token = r'\includegraphics'  # noqa
+    pos_after = len(token)
+    return len(text) > len(token) and text.startswith(token) and text[pos_after] in ('[', '{')
+
+
+def extract_image_path(include_graphics_line: str) -> str:
+    """We had a bug, so we isolate in a function."""
+    return include_graphics_line.split('{', 1)[1].rstrip().rstrip('}')
+
+
 def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) -> list[str]:
     """Later alligator."""
     outgoing = []
@@ -16,15 +28,16 @@ def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) 
     precondition = r'\begin{figure}'
     precondition_met = False
     for slot, line in enumerate(incoming):
+        if line.startswith(precondition) and not precondition_met:
+            log.info(f'start of a figure environment at line #{slot + 1}')
+            precondition_met = True
+            outgoing.append(line)
+            continue
+
         if modus == 'copy':
-            if line.startswith(precondition):
-                log.info(f'start of a figure environment at line #{slot + 1}')
-                precondition_met = True
-                outgoing.append(line)
-                continue
-            if line.startswith(r'\includegraphics{') and not precondition_met:
+            if is_include_graphics(line) and not precondition_met:
                 log.warning(f'graphics include outside of a figure environment at line #{slot + 1}')
-                log.error(f'line#{slot + 1}|{line}')
+                log.error(f'line#{slot + 1}|{line.rstrip()}')
                 log.info('trying to fix temporarily ... watch for marker MISSING-CAPTION-IN-MARKDOWN')
                 adhoc_label = 'FIX-AT-SOURCE'
                 try:
@@ -37,9 +50,17 @@ def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) 
                     log.error(f'failed to extract generic label from {line.strip()} with err: {err}')
                 captain = 'MISSING-CAPTION-IN-MARKDOWN'
                 try:
-                    token = line.split('{', 1)[1].rstrip('}')
+                    token = extract_image_path(line)
+                    log.debug(f'- looking up key ({token}) for captions ...')
                     if lookup is not None:
-                        captain = lookup.get(token, captain)
+                        cand = lookup.get(token, None)
+                        if cand is not None:
+                            captain = cand
+                            log.debug(f'  + found ({captain})')
+                        else:
+                            log.debug(f'  ? no match for key ({token})')
+                    else:
+                        log.debug('  + no lut?')
                 except Exception as err:
                     log.error(
                         f'failed to extract file path token for caption lookup from {line.strip()} with err: {err}'
@@ -50,9 +71,9 @@ def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) 
                 outgoing.append(line)
                 outgoing.append(r'\caption{' + captain + ' ' + adhoc_label + '}')
                 outgoing.append(r'\end{figure}')
-            elif line.startswith(r'\includegraphics{') and precondition_met:
+            elif is_include_graphics(line) and precondition_met:
                 log.info(f'within a figure environment at line #{slot + 1}')
-                log.info(line)
+                log.info(line.rstrip())
                 modus = 'figure'
                 figure = [line]
                 try:
@@ -65,9 +86,11 @@ def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) 
                     log.error(f'failed to extract generic label from {line.strip()} with err: {err}')
                 caption = []
             else:
+                log.debug(f'- copying {slot + 1 :3d}|{line.rstrip()}')
                 outgoing.append(line)
+            continue
 
-        elif modus == 'figure':
+        if modus == 'figure':
             if line.startswith(r'\caption{'):
                 log.info(f'- found the caption start at line #{slot + 1}')
                 caption.append(line)
@@ -90,11 +113,13 @@ def inject(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) 
             else:
                 log.debug(f'- figure continues at line #{slot + 1}')
                 figure.append(line)
+            continue
 
-        elif modus == 'caption':
+        if modus == 'caption':
             caption.append(line)
             if line.strip().endswith(r'}'):
                 log.info(f'- caption read at line #{slot + 1}')
                 modus = 'figure'
+            continue
 
     return outgoing
