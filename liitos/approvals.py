2f931cc15b8d6fc72268e4e16cd94a76448f5703
Stefan Hagen <stefan@hagen.link>
2024-01-17 20:54:51 +0100

    Refactored approvals module
    - Better testability (tests are WIP)
    - Added documentation on future layouts
    - Prepared layout switch handling
    
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- liitos/approvals.py
+++ liitos/approvals.py
@@ -1,11 +1,34 @@
-"""Weave the content of the approvals data file into the output structure (for now LaTeX)."""
+"""Weave the content of the approvals data file into the output structure (for now LaTeX).
+
+# Supported Table Layouts
+
+# Layout `south` (the old default)
+
+| Approvals  | Name         | Date and Signature |
+|:-----------|:-------------|:-------------------|
+| Author     | Au. Thor.    |                    |
+| Reviewer   | Re. Viewer.  |                    |
+| Approver   | Ap. Prover.  |                    |
+| Authorizer | Au. Thorizer |                    |
+
+# Layout `east` (the new default)
+
+| Department          |    AB    |     AB     |     AB     |     AB.      |
+|:--------------------|:--------:|:----------:|:----------:|:------------:|
+| Approvals           |  Author  |  Reviewer  |  Approver  |  Authorizer  |
+| Name                | Au. Thor | Re. Viewer | Ap. Prover | Au. Thorizer |
+| Date <br> Signature |          |            |            |              |
+
+"""
 import os
 import pathlib
-from typing import Union
+from typing import Union, no_type_check
 
 import liitos.gather as gat
 import liitos.template_loader as template
 from liitos import ENCODING, LOG_SEPARATOR, log
+
+PathLike = Union[str, pathlib.Path]
 
 BOOKMATTER_TEMPLATE = os.getenv('LIITOS_BOOKMATTER_TEMPLATE', '')
 BOOKMATTER_TEMPLATE_IS_EXTERNAL = bool(BOOKMATTER_TEMPLATE)
@@ -26,6 +49,69 @@ CUT_MARKER_TOP = '% |-- approvals - cut - marker - top -->'
 CUT_MARKER_BOTTOM = '% <-- approvals - cut - marker - bottom --|'
 
 
+def get_layout(layout_path: PathLike) -> dict[str, dict[str, dict[str, bool]]]:
+    """DRY."""
+    layout = {'layout': {'global': {'has_approvals': True, 'has_changes': True, 'has_notices': True}}}
+    if layout_path:
+        log.info(f'loading layout from {layout_path=} for approvals')
+        layout = gat.load_layout(facet_key, target_key, layout_path)[0]  # type: ignore
+    else:
+        log.info('using default layout for approvals')
+    return layout
+
+
+def derive_model(model_path: PathLike) -> tuple[str, list[str]]:
+    """Derive the model as channel type and column model from the given path."""
+    channel = JSON_CHANNEL if str(model_path).endswith('.json') else YAML_CHANNEL
+    columns_expected = ['Approvals', 'Name'] if channel == JSON_CHANNEL else COLUMNS_EXPECTED
+
+    return channel, columns_expected
+
+
+@no_type_check
+def normalize(signatures: object, channel: str, columns_expected: list[str]) -> list[dict[str, str]]:
+    """Normalize the channel specific topology of the model into a logical model.
+
+    On error an empty logical model is returned.
+    """
+    if channel == JSON_CHANNEL:
+        if signatures[0]['columns'] != columns_expected:
+            log.error('unexpected column model!')
+            log.error(f'-  expected: ({columns_expected})')
+            log.error(f'- but found: ({signatures[0]["columns"]})')
+            return []
+
+    if channel == YAML_CHANNEL:
+        for slot, approval in enumerate(signatures[0]['approvals'], start=1):
+            log.debug(f'{slot=}, {approval=}')
+            if sorted(approval) != sorted(columns_expected):
+                log.error('unexpected column model!')
+                log.error(f'-  expected: ({columns_expected})')
+                log.error(f'- but found: ({sorted(approval)}) in slot #{slot}')
+                return []
+
+    if channel == JSON_CHANNEL:
+        return [{'role': role, 'name': name} for role, name in signatures[0]['rows']]
+
+    return [{'role': approval['role'], 'name': approval['name']} for approval in signatures[0]['approvals']]
+
+
+@no_type_check
+def remove_target_region_gen(text_lines: list[str]):
+    """Return generator that yields only the lines beyond the cut mark region."""
+    in_section = False
+    for line in text_lines:
+        if not in_section:
+            if CUT_MARKER_TOP in line:
+                in_section = True
+                continue
+        if in_section:
+            if CUT_MARKER_BOTTOM in line:
+                in_section = False
+            continue
+        yield line
+
+
 def weave(
     doc_root: Union[str, pathlib.Path],
     structure_name: str,
@@ -44,25 +130,15 @@ def weave(
         command='approvals',
     )
 
-    layout = {'layout': {'global': {'has_approvals': True, 'has_changes': True, 'has_notices': True}}}
     layout_path = asset_map[target_key][facet_key].get(gat.KEY_LAYOUT, '')
-    if layout_path:
-        log.info(f'loading layout from {layout_path=} for approvals')
-        layout = gat.load_layout(facet_key, target_key, layout_path)[0]  # type: ignore
-    else:
-        log.info('using default layout for approvals')
+    layout = get_layout(layout_path)
     log.info(f'{layout=}')
 
     log.info(LOG_SEPARATOR)
-
-    channel = YAML_CHANNEL
-    columns_expected = COLUMNS_EXPECTED
     signatures_path = asset_map[target_key][facet_key][gat.KEY_APPROVALS]
-    if str(signatures_path).endswith('.json'):
-        channel = JSON_CHANNEL
-        columns_expected = ['Approvals', 'Name']
-
+    channel, columns_expected = derive_model(signatures_path)
     log.info(f'detected approvals channel ({channel}) weaving in from ({signatures_path})')
+
     log.info(f'loading signatures from {signatures_path=}')
     signatures = gat.load_approvals(facet_key, target_key, signatures_path)
     log.info(f'{signatures=}')
@@ -70,29 +146,9 @@ def weave(
     log.info(LOG_SEPARATOR)
     log.info('plausibility tests for approvals ...')
 
-    rows = []
-    if channel == JSON_CHANNEL:
-        if signatures[0]['columns'] != columns_expected:
-            log.error('unexpected column model!')
-            log.error(f'-  expected: ({columns_expected})')
-            log.error(f'- but found: ({signatures[0]["columns"]})')
-            return 1
+    logical_model = normalize(signatures, channel=channel, columns_expected=columns_expected)
 
-        for role, name in signatures[0]['rows']:  # type: ignore
-            rows.append(ROW_TEMPLATE.replace('role', role).replace('name', name))
-    else:
-        for slot, approval in enumerate(signatures[0]['approvals'], start=1):
-            log.debug(f'{slot=}, {approval=}')
-            if sorted(approval) != sorted(columns_expected):
-                log.error('unexpected column model!')
-                log.error(f'-  expected: ({columns_expected})')
-                log.error(f'- but found: ({sorted(approval)}) in slot #{slot}')
-                return 1
-
-        for approval in signatures[0]['approvals']:
-            role = approval['role']  # type: ignore
-            name = approval['name']  # type: ignore
-            rows.append(ROW_TEMPLATE.replace('role', role).replace('name', name))
+    rows = [ROW_TEMPLATE.replace('role', kv['role']).replace('name', kv['name']) for kv in logical_model]
 
     pushdown = EXTRA_OFFSET_EM - 2 * len(rows)
     log.info(f'calculated extra pushdown to be {pushdown}em')
@@ -102,19 +158,7 @@ def weave(
 
     if not layout['layout']['global']['has_approvals']:
         log.info('removing approvals from document layout')
-        in_section = False
-        keep = []
-        for line in lines:
-            if not in_section:
-                if CUT_MARKER_TOP in line:
-                    in_section = True
-                    continue
-            if in_section:
-                if CUT_MARKER_BOTTOM in line:
-                    in_section = False
-                continue
-            keep.append(line)
-        lines = keep
+        lines = list(remove_target_region_gen(lines))
 
     log.info(LOG_SEPARATOR)
     log.info(f'weaving in the approvals from {signatures_path}...')
