c87a3c96eda0c6345d6c2bed1451e9203471df37
Stefan Hagen <stefan@hagen.link>
2024-01-21 14:30:42 +0100

    Extended template handling tests
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- test/test_template_loader.py
+++ test/test_template_loader.py
@@ -1,7 +1,10 @@
+import os
+import pathlib
 import tempfile
 
 import liitos.template_loader as template
 
+ALIEN_PATH = pathlib.Path('test', 'fixtures', 'templates', 'alien')
 META_PATCH_CONTENT = """\
 ---
 document:
@@ -16,6 +19,18 @@ def test_load_meta_patch():
     assert template.load_resource('templates/meta-patch.yml', False) == META_PATCH_CONTENT
 
 
+def test_load_alien():
+    assert int(template.load_resource(ALIEN_PATH, True).strip()) == 42
+
+
 def test_eject():
     with tempfile.TemporaryDirectory() as tmpdirname:
         assert template.eject([tmpdirname]) == 0
+
+
+def test_eject_default():
+    prior = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.chdir(tmpdirname)
+        assert template.eject() == 0
+    os.chdir(prior)
