cd61ab9fb9b5141630aec3f9fff7ef2ecdd31410
Stefan Hagen <stefan@hagen.link>
2024-01-22 22:35:15 +0100

    Added placeholder module tests
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- test/test_placeholder_loader.py
+++ test/test_placeholder_loader.py
@@ -6,10 +6,17 @@ import liitos.placeholder_loader as placeholder
 from liitos import ENCODING
 
 ALIEN_PATH = pathlib.Path('test', 'fixtures', 'templates', 'alien')
+EXTERNAL_PATH = pathlib.Path('test', 'fixtures', 'placeholder', 'this-resource-is-missing.svg')
 
 
 def test_load_svg_placeholder():
     kind, data = placeholder.load_resource('placeholders/this-resource-is-missing.svg', False)
+    assert kind == 'str'
+    assert isinstance(data, str)
+
+
+def test_load_svg_placeholder_external():
+    kind, data = placeholder.load_resource(EXTERNAL_PATH, True)
     assert kind == 'str'
     assert isinstance(data, str)
 
@@ -32,3 +39,36 @@ def test_eject_default():
         os.chdir(tmpdirname)
         assert placeholder.eject() == 0
     os.chdir(prior)
+
+
+def test_dump_placeholder_text():
+    base = 'foo'
+    suffix = '.svg'
+    target_name = f'{base}{suffix}'
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        code, msg = placeholder.dump_placeholder(pathlib.Path(tmpdirname) / target_name)
+    assert code == 0
+    assert msg.startswith(f'wrote text mode placeholder matching suffix ({suffix}) derived from target(')
+    assert msg.endswith(f'/{target_name})')
+
+
+def test_dump_placeholder_binary():
+    base = 'foo'
+    suffix = '.png'
+    target_name = f'{base}{suffix}'
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        code, msg = placeholder.dump_placeholder(pathlib.Path(tmpdirname) / target_name)
+    assert code == 0
+    assert msg.startswith(f'wrote binary mode placeholder matching suffix ({suffix}) derived from target(')
+    assert msg.endswith(f'/{target_name})')
+
+
+def test_dump_placeholder_miss():
+    base = 'foo'
+    suffix = '.does-not-exist'
+    target_name = f'{base}{suffix}'
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        code, msg = placeholder.dump_placeholder(pathlib.Path(tmpdirname) / target_name)
+    assert code == 1
+    assert msg.startswith(f'no placeholder found matching suffix ({suffix}) derived from target(')
+    assert msg.endswith(f'/{target_name})')
