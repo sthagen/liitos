fa73a3edb1eef7f30d33951f9b37f18cd2f42296
Stefan Hagen <stefan@hagen.link>
2024-11-06 22:20:35 +0100

    Bumped implementation version
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- liitos/__init__.py
+++ liitos/__init__.py
@@ -8,8 +8,8 @@ import shellingham  # type: ignore
 from typing import Union, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2024.10.29+parent.g1916640f'
-# [[[end]]] (checksum: 045bb2fe3e25659a4926fd6ee073b660)
+__version__ = '2024.11.6+parent.g82650d35'
+# [[[end]]] (checksum: 83e1dc58fbbf7eb71f615e45238b623f)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
