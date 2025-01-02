e66017e53484d87bba541fbaaed58f63b9fd83fe
Stefan Hagen <stefan@hagen.link>
2025-01-02 23:55:54 +0100

    Refactored description lists module
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- liitos/description_lists.py
+++ liitos/description_lists.py
@@ -13,32 +13,77 @@ Modus = Enum('Modus', 'COPY OPTION')
 NO_OPTION: str = ''
 
 
-def parse_option_command(slot: int, text_line: str) -> tuple[bool, str, str]:
-    r"""Parse the \option[style=multiline,leftmargin=6em].
+def filter_seek_option(line: str, slot: int, modus: Modus, opt: str, outgoing: list[str]) -> tuple[Modus, str]:
+    r"""Filter line, seek for an option command, and return updated mnodus, opt pair.
 
     Examples:
 
-    >>> in_opts = '[style=multiline,leftmargin=6em]'
-    >>> line = rf'\option{in_opts}'
-    >>> ok, processed, out_opts = parse_option_command(42, line)
-    >>> assert ok
-    >>> assert processed.startswith('%CONSIDERED_')
-    >>> assert out_opts == in_opts
+    >>> filtered = []
+    >>> m, opt = filter_seek_option(r'\option[]', 0, Modus.COPY, NO_OPTION, filtered)
+    >>> assert not filtered
+    >>> assert m == Modus.OPTION
+    >>> assert opt == '[]'
 
-    >>> line = 'foo'
-    >>> ok, processed, out_opts = parse_option_command(-1, line)
-    >>> assert not ok
-    >>> assert processed == line
-    >>> assert out_opts == ''
+    >>> filtered = []
+    >>> m, opt = filter_seek_option('foo', 0, Modus.COPY, NO_OPTION, filtered)
+    >>> assert filtered == ['foo']
+    >>> assert m == Modus.COPY
+    >>> assert opt == NO_OPTION
+
+    >>> filtered = []
+    >>> m, opt = filter_seek_option(r'\option[foo=bar]', 0, Modus.COPY, NO_OPTION, filtered)
+    >>> assert not filtered
+    >>> assert m == Modus.OPTION
+    >>> assert opt == '[foo=bar]'
     """
-    if text_line.startswith(r'\option['):
-        log.info(f'trigger an option mod for the next description environment at line #{slot + 1}|{text_line}')
+    if line.startswith(r'\option['):
+        log.info(f'trigger an option mod for the next description environment at line #{slot + 1}|{line}')
         # \option[style=multiline,leftmargin=6em]  --> [style=multiline,leftmargin=6em]
-        opt = text_line.split(r'\option', 1)[1].strip()
+        opt = line.split(r'\option', 1)[1].strip()
+        modus = Modus.OPTION
         log.info(f' -> parsed option as ({opt})')
-        return True, f'%CONSIDERED_{text_line}', opt
+        # return True, f'%CONSIDERED_{line}', opt
     else:
-        return False, text_line, ''
+        outgoing.append(line)
+
+    return modus, opt
+
+
+def filter_seek_description(line: str, slot: int, modus: Modus, opt: str, outgoing: list[str]) -> tuple[Modus, str]:
+    r"""Filter line, seek for a description, add options if applicable, and return updated mnodus, option pair.
+
+    Examples:
+
+    >>> filtered = []
+    >>> m, opt = filter_seek_description('quux', 0, Modus.OPTION, '[foo=bar]', filtered)
+    >>> assert filtered == ['quux']
+    >>> assert m == Modus.OPTION
+    >>> assert opt == '[foo=bar]'
+
+    >>> filtered = []
+    >>> m, opt = filter_seek_description(r'\begin{description}', 0, Modus.OPTION, NO_OPTION, filtered)
+    >>> assert filtered == [r'\begin{description}']
+    >>> assert m == Modus.COPY
+    >>> assert opt == NO_OPTION
+
+    >>> filtered = []
+    >>> m, opt = filter_seek_description(r'\begin{description}', 0, Modus.OPTION, '[foo=bar]', filtered)
+    >>> assert filtered == [r'\begin{description}[foo=bar]']
+    >>> assert m == Modus.COPY
+    >>> assert opt == NO_OPTION
+    """
+    if line.startswith(r'\begin{description}'):
+        if opt != NO_OPTION:
+            log.info(f'- found the option target start at line #{slot + 1}|{line}')
+            outgoing.append(f'\\begin{{description}}{opt}')
+        else:
+            outgoing.append(line)
+        modus = Modus.COPY
+        opt = NO_OPTION
+    else:
+        outgoing.append(line)
+
+    return modus, opt
 
 
 def options(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) -> list[str]:
@@ -56,28 +101,13 @@ def options(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None)
     >>> processed
     ['a', '', '', '\\begin{description}[style=multiline,leftmargin=6em]', 'whatever']
     """
-    outgoing = []
+    outgoing: list[str] = []
     modus = Modus.COPY
     opt = NO_OPTION
     for slot, line in enumerate(incoming):
         if modus == Modus.COPY:
-            has_opt, text_line, opt = parse_option_command(slot, line)
-            if has_opt:
-                modus = Modus.OPTION
-            else:
-                outgoing.append(text_line)
-            continue
-
-        # if modus == 'option':
-        if line.startswith(r'\begin{description}'):
-            if opt != NO_OPTION:
-                log.info(f'- found the option target start at line #{slot + 1}|{line}')
-                outgoing.append(f'\\begin{{description}}{opt}')
-            else:
-                outgoing.append(line)
-            modus = Modus.COPY
-            opt = NO_OPTION
-        else:
-            outgoing.append(line)
+            modus, opt = filter_seek_option(line, slot, modus, opt, outgoing)
+        else:  # if modus == Modus.OPTION:
+            modus, opt = filter_seek_description(line, slot, modus, opt, outgoing)
 
     return outgoing
