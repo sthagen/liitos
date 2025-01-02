725aea56e17b79581928676cca70d848ed2bd78b
Stefan Hagen <stefan@hagen.link>
2025-01-02 18:39:55 +0100

    Migrated to enums for modus operandi in figures module
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- liitos/figures.py
+++ liitos/figures.py
@@ -1,38 +1,40 @@
 from collections.abc import Iterable
+from enum import Enum
 import math
 from typing import Union
 
 from liitos import log
 
+Modus = Enum('Modus', [('COPY', 1), ('SCALE', 2)])
 NAN = float('nan')
 
 
-def filter_seek_scale(line: str, slot: int, modus: str, rescale: float, outgoing: list[str]) -> tuple[str, float]:
+def filter_seek_scale(line: str, slot: int, modus: Modus, rescale: float, outgoing: list[str]) -> tuple[Modus, float]:
     r"""Filter line, seek for a scale command, and return updated mnodus, rescale pair.
 
     Examples:
 
     >>> o = []
-    >>> m, r = filter_seek_scale(r'\scale=', 0, 'copy', NAN, o)
+    >>> m, r = filter_seek_scale(r'\scale=', 0, Modus.COPY, NAN, o)
     >>> assert not o
-    >>> assert m == 'scale'
+    >>> assert m == Modus.SCALE
     >>> assert math.isnan(r)
 
     >>> o = []
-    >>> m, r = filter_seek_scale('foo', 0, 'copy', NAN, o)
+    >>> m, r = filter_seek_scale('foo', 0, Modus.COPY, NAN, o)
     >>> assert o == ['foo']
-    >>> assert m == 'copy'
+    >>> assert m == Modus.COPY
     >>> assert math.isnan(r)
 
     >>> o = []
-    >>> m, r = filter_seek_scale(r'\scale=80\%', 0, 'copy', NAN, o)
+    >>> m, r = filter_seek_scale(r'\scale=80\%', 0, Modus.COPY, NAN, o)
     >>> assert not o
-    >>> assert m == 'scale'
+    >>> assert m == Modus.SCALE
     >>> assert r == 0.8
     """
     if line.startswith(r'\scale='):
         log.info(f'trigger a scale mod for the next figure environment at line #{slot + 1}|{line}')
-        modus = 'scale'
+        modus = Modus.SCALE
         scale = line  # only for reporting will not pass the filter
         try:
             sca = scale.split('=', 1)[1].strip()  # \scale    =    75\%  --> 75\%
@@ -45,35 +47,35 @@ def filter_seek_scale(line: str, slot: int, modus: str, rescale: float, outgoing
     return modus, rescale
 
 
-def filter_seek_figure(line: str, slot: int, modus: str, rescale: float, outgoing: list[str]) -> tuple[str, float]:
+def filter_seek_figure(line: str, slot: int, modus: Modus, rescale: float, outgoing: list[str]) -> tuple[Modus, float]:
     r"""Filter line, seek for a figure, rescale if applicable, and return updated mnodus, rescale pair.
 
     Examples:
 
     >>> o = []
-    >>> m, r = filter_seek_figure(r'\includegraphics{', 0, 'copy', NAN, o)
+    >>> m, r = filter_seek_figure(r'\includegraphics{', 0, Modus.COPY, NAN, o)
     >>> assert o == [r'\includegraphics{']
-    >>> assert m == 'copy'
+    >>> assert m == Modus.COPY
     >>> assert math.isnan(r)
 
     >>> o = []
-    >>> m, r = filter_seek_figure('foo', 0, 'copy', 0.8, o)
+    >>> m, r = filter_seek_figure('foo', 0, Modus.COPY, 0.8, o)
     >>> assert o == ['foo']
-    >>> assert m == 'copy'
+    >>> assert m == Modus.COPY
     >>> assert r == 0.8
 
     >>> o = []
     >>> rescale = 0.8
-    >>> m, r = filter_seek_figure(r'\pandocbounded{\includegraphics', 0, 'copy', rescale, o)
+    >>> m, r = filter_seek_figure(r'\pandocbounded{\includegraphics', 0, Modus.COPY, rescale, o)
     >>> assert o[0].startswith(r'\pandocbounded{\includegraphics')
     >>> assert f'textwidth,height={rescale}' in o[0]
-    >>> assert m == 'copy'
+    >>> assert m == Modus.COPY
     >>> assert math.isnan(r)
 
     >>> o = []
-    >>> m, r = filter_seek_figure(r'\pandocbounded{\includegraphics', 0, 'copy', NAN, o)
+    >>> m, r = filter_seek_figure(r'\pandocbounded{\includegraphics', 0, Modus.COPY, NAN, o)
     >>> assert o[0].startswith(r'\pandocbounded{\includegraphics')
-    >>> assert m == 'copy'
+    >>> assert m == Modus.COPY
     >>> assert math.isnan(r)
     """
     if line.startswith(r'\includegraphics{'):
@@ -84,7 +86,7 @@ def filter_seek_figure(line: str, slot: int, modus: str, rescale: float, outgoin
             outgoing.append(f'\\includegraphics{option}{target}')
         else:
             outgoing.append(line)
-        modus = 'copy'
+        modus = Modus.COPY
         rescale = NAN
     elif r'\pandocbounded{\includegraphics' in line:
         if not math.isnan(rescale):
@@ -100,7 +102,7 @@ def filter_seek_figure(line: str, slot: int, modus: str, rescale: float, outgoin
             outgoing.append(f'\\pandocbounded{{\\includegraphics{option}{inside}}}{rest}')
         else:
             outgoing.append(line)
-        modus = 'copy'
+        modus = Modus.COPY
         rescale = NAN
     else:
         outgoing.append(line)
@@ -125,13 +127,13 @@ def scale(incoming: Iterable[str], lookup: Union[dict[str, str], None] = None) -
     ['foo', '', '\\includegraphics{', '', 'quux']
     """
     outgoing: list[str] = []
-    modus = 'copy'
+    modus = Modus.COPY
     rescale = NAN
     for slot, line in enumerate(incoming):
         line = line.rstrip('\n')
-        if modus == 'copy':
+        if modus == Modus.COPY:
             modus, rescale = filter_seek_scale(line, slot, modus, rescale, outgoing)
-        else:  # if modus == 'scale':
+        else:  # if modus == Modus.SCALE:
             modus, rescale = filter_seek_figure(line, slot, modus, rescale, outgoing)
 
     return outgoing
